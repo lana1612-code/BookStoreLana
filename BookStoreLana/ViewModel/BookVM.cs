072e370d0011fddf5b29762917030bcf683b6941
lana Hasan <lanahasan1712@gmail.com>
2024-06-05 13:26:26 +0300

    session6

---
+++ BookStoreLana/ViewModel/BookVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreLana.ViewModel
+{
+	public class BookVM
+	{
+		public int Id { get; set; }
+		public string Title { get; set; } = null!;
+		public string Auther { get; set; } = null!;
+		public string Publisher { get; set; } = null!;
+
+		public DateTime publishDate { get; set; } 
+		public string ImageUrl { get; set; }
+		public List<string> Categories { get; set; } 
+	}
+}
